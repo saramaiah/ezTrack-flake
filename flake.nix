a29f7ab07b20dc3ccd5daeeceeb0a0e11e61a14d
saramaiah <s.a.ramaiah27@gmail.com>
2025-03-23 14:11:02 -0400

    Initial flake defining packages necessary for ezTrack algorithms.

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A flake for running video analysis with ezTrack.";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+  }:
+    let
+      supportedSystems = [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      eachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f (import nixpkgs {inherit system;}));
+    in {
+      packages = eachSupportedSystem (pkgs: {
+        python = pkgs.python313;
+        jupyter = pkgs.python313Packages.jupyter;
+        numpy = pkgs.python313Packages.numpy;
+        scipy = pkgs.python313Packages.scipy;
+        pandas = pkgs.python313Packages.pandas;
+        opencv = pkgs.python313Packages.opencv4;
+        holoviews = pkgs.python313Packages.holoviews;
+        bokeh = pkgs.python313Packages.bokeh;
+        pyviz-comms = pkgs.python313Packages.pyviz-comms;
+        jinja2 = pkgs.python313Packages.jinja2;
+        scikit-learn = pkgs.python313Packages.scikit-learn;
+        matplotlib = pkgs.python313Packages.matplotlib;
+        tqdm = pkgs.python313Packages.tqdm;
+      });
+    };
+}
+
