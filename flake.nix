a71f20435cb2cdd0695894ce0b7e3517ff76e427
saramaiah <s.a.ramaiah27@gmail.com>
2025-03-23 16:27:03 -0400

    Changed packages to be latest compatible with python 3.9, removed pycryptodome

--- flake.nix
+++ flake.nix
@@ -18,19 +18,18 @@
     in {
       packages = eachSupportedSystem (pkgs: {
         python = pkgs.python39;
-        jupyter = pkgs.jupyter-all;
-        numpy = pkgs.python313Packages.numpy;
-        scipy = pkgs.python313Packages.scipy;
-        pandas = pkgs.python313Packages.pandas;
-        opencv = pkgs.python313Packages.opencv4;
-        holoviews = pkgs.python313Packages.holoviews;
-        bokeh = pkgs.python313Packages.bokeh;
-        pyviz-comms = pkgs.python313Packages.pyviz-comms;
-        jinja2 = pkgs.python313Packages.jinja2;
-        scikit-learn = pkgs.python313Packages.scikit-learn;
-        matplotlib = pkgs.python313Packages.matplotlib;
-        tqdm = pkgs.python313Packages.tqdm;
-        pycryptodome = pkgs.python313Packages.pycryptodome;
+        jupyter = pkgs.python3.9-jupyter;
+        numpy = pkgs.python3.9-numpy;
+        scipy = pkgs.python3.9-scipy;
+        pandas = pkgs.python3.9-pandas;
+        opencv = pkgs.python3.9-opencv4;
+        holoviews = pkgs.python3.9-holoviews;
+        bokeh = pkgs.python3.9-bokeh;
+        pyviz-comms = pkgs.python3.9-pyviz-comms;
+        jinja2 = pkgs.python3.9-jinja2;
+        scikit-learn = pkgs.python3.9-scikit-learn;
+        matplotlib = pkgs.python3.9-matplotlib;
+        tqdm = pkgs.python3.9-tqdm;
       });
     };
 }
